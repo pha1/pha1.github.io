a99c105b2b99ea975c20e376c3db8522ec47ae8e
Phi <pha1@uncc.edu>
2021-11-10 19:12:51 -0500

    Activity 11 and Activity 12 Added to Webpages
    Activity 11 and 12 completed and added to webpages. Created a navigation bar component for itis 3135 webpages.

---
+++ itis3135/Activity11/Ha-Activity11.js
@@ -0,0 +1,7 @@
+<!-- jQuery call to the accordion() method. -->
+$(document).ready(function() {
+    $("#accordion").accordion({
+        collapsible: true,
+        heightStyle: "content"
+    });
+});
