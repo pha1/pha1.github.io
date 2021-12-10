d7da683c88f5434eb7cb5d43aac493385d7da06e
Phi <pha1@uncc.edu>
2021-12-09 21:59:10 -0500

    Added Validation links
    Added validation links to all pages and corrected any errors that were repoted.

--- itis3135/project/scripts/billingConfirmation.js
+++ itis3135/project/scripts/billingConfirmation.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
     $("#billConfirm").append(
-        "<h2> Thank you for purchasing from Quality Auto Wash, " + localStorage.getItem("first_name") + "!</h2>" +
+        "<h2> Thank you for purchasing from Quality Auto Wash, " + localStorage.getItem("bill_first_name") + "!</h2>" +
         "<p>This is just a confirmation page to show that you have made a purchase with us! We would like for you to review the information that was entered in case of any issues:</p>" +
         "<p>Name: " + localStorage.getItem("bill_first_name") + " " + localStorage.getItem("bill_last_name") + "</p>" +
         "<p>Email Address: " + localStorage.getItem("bill_email_address") + "</p>" +
