7048b143588abb6b7617a08a955ee5d8c5eb0b96
pha1 <89549278+pha1@users.noreply.github.com>
2022-10-26 14:21:12 -0400

    Submit function 10/26/22
    Submit button should be updated. No longer automatically load all data into the page upon entering the webpage.

--- NCDOI/scripts/inspectors.js
+++ NCDOI/scripts/inspectors.js
@@ -1,95 +1,93 @@
-<script>
 
-            /* toggle between hiding and showing the dropdown content */
-            function selectTrade() {
-              document.getElementById("tradeDropdown").classList.toggle("show");
-            }
+/* toggle between hiding and showing the dropdown content */
+function selectTrade() {
+    document.getElementById("tradeDropdown").classList.toggle("show");
+}
 
-            function selectLevel() {
-              document.getElementById("levelDropdown").classList.toggle("show");
-            }
+function selectLevel() {
+    document.getElementById("levelDropdown").classList.toggle("show");
+}
 
-            // Close the dropdown if the user clicks outside of it
-            window.onclick = function(event) {
-              if (!event.target.matches('.dropbtn')) {
-                var dropdowns = document.getElementsByClassName("dropdown-content");
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                  var openDropdown = dropdowns[i];
-                  if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                  }
-                }
-              }
-            }
-              $(document).ready(function () {
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        }
+    }
+    }
+}
+    $(document).ready(function () {
 
-                  // FETCHING DATA FROM JSON FILE
-                  $.getJSON("inspectors.json", 
-                          function (data) {
-                      var inspector = '';
+        // FETCHING DATA FROM JSON FILE
+        $.getJSON("inspectors.json", 
+                function (data) {
+            var inspector = '';
 
-                      // ITERATING THROUGH OBJECTS
-                      $.each(data, function (key, value) {
+            // ITERATING THROUGH OBJECTS
+            $.each(data, function (key, value) {
 
-                          //CONSTRUCTION OF ROWS HAVING
-                          // DATA FROM JSON OBJECT
-                          inspector += '<tr>';
+                //CONSTRUCTION OF ROWS HAVING
+                // DATA FROM JSON OBJECT
+                inspector += '<tr>';
 
-                          // To get values with spaces, use bracket notation
-                          inspector += '<td>' + 
-                              value["CEO ID"] + '</td>';
+                // To get values with spaces, use bracket notation
+                inspector += '<td>' + 
+                    value["CEO ID"] + '</td>';
 
-                          inspector += '<td>' + 
-                              value["First Name"] + '</td>';
+                inspector += '<td>' + 
+                    value["First Name"] + '</td>';
 
-                          inspector += '<td>' + 
-                              value["Last Name"] + '</td>';
+                inspector += '<td>' + 
+                    value["Last Name"] + '</td>';
 
-                          inspector += '<td>' + 
-                              value["Cert No"] + '</td>';
+                inspector += '<td>' + 
+                    value["Cert No"] + '</td>';
 
-                          inspector += '<td>' + 
-                              value.Type + '</td>';
+                inspector += '<td>' + 
+                    value.Type + '</td>';
 
-                          inspector += '<td>' + 
-                              value.Trade + '</td>';
+                inspector += '<td>' + 
+                    value.Trade + '</td>';
 
-                          inspector += '<td>' + 
-                              value.Level + '</td>';
+                inspector += '<td>' + 
+                    value.Level + '</td>';
 
-                          inspector += '<td>' + 
-                              value["Exp Date"] + '</td>';
+                inspector += '<td>' + 
+                    value["Exp Date"] + '</td>';
 
-                          inspector += '<td>' + 
-                              value.Employer + '</td>';
+                inspector += '<td>' + 
+                    value.Employer + '</td>';
 
-                          inspector += '<td>' + 
-                              value["Emp Phone"] + '</td>';
+                inspector += '<td>' + 
+                    value["Emp Phone"] + '</td>';
 
-                          inspector += '<td>' + 
-                              value.County + '</td>';
+                inspector += '<td>' + 
+                    value.County + '</td>';
 
-                          inspector += '<td>' + 
-                              value.Address + '</td>';
+                inspector += '<td>' + 
+                    value.Address + '</td>';
 
-                          inspector += '<td>' + 
-                              value.City + '</td>';
+                inspector += '<td>' + 
+                    value.City + '</td>';
 
-                          inspector += '<td>' + 
-                              value.State + '</td>';
+                inspector += '<td>' + 
+                    value.State + '</td>';
 
-                          inspector += '<td>' + 
-                              value["Zip Code"] + '</td>';
+                inspector += '<td>' + 
+                    value["Zip Code"] + '</td>';
 
-                          inspector += '<td>' + 
-                              value["Primary contact email"] + '</td>';
+                inspector += '<td>' + 
+                    value["Primary contact email"] + '</td>';
 
-                          inspector += '</tr>';
-                      });
-                        
-                      //INSERTING ROWS INTO TABLE 
-                      $('#inspector_table').append(inspector);
-                  });
-              });
-          </script>
+                inspector += '</tr>';
+            });
+            
+            //INSERTING ROWS INTO TABLE 
+            $('#inspector_table').append(inspector);
+        });
+    });
