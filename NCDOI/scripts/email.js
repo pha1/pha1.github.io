1b700e82352aa3a47aae41f84180e71a16c79d0f
pha1 <89549278+pha1@users.noreply.github.com>
2022-11-16 14:56:48 -0500

    Update email.js

--- NCDOI/scripts/email.js
+++ NCDOI/scripts/email.js
@@ -4,12 +4,14 @@ src="inspector.js";
 
 function emailList() {
   console.log("kek");
+  /*
     var selectedValues = $('input.chk:checked').toArray().map(function(chk) {
       return $(chk).val();
     });
   
     console.log(selectedValues.join());
   };
+  */
 
 function sendEmail() {
     
