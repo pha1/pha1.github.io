f7398122b782b94725c3df993dabd141b34f410e
pha1 <89549278+pha1@users.noreply.github.com>
2022-11-17 18:49:21 -0500

    Update mapAPI.js

--- NCDOI/scripts/mapAPI.js
+++ NCDOI/scripts/mapAPI.js
@@ -16,5 +16,46 @@ function initMap() {
         }
     });
     map.data.loadGeoJson('files/NC_Counties.geojson');
+
+    const input = document.getElementById("searchLocation");
+    const options = {
+        fields: ["formatted_address", "geometry", "name"],
+        strictBounds: false,
+        types: ["address"],
+      };
+
+    const autocomplete = new google.maps.places.Autocomplete(input, options);
+    
+    autocomplete.bindTo("bounds", map);
+
+    const marker = new google.maps.Marker({
+        map,
+        anchorPoint: new google.maps.Point(0, -29),
+    });
+
+    autocomplete.addListener("place_changed", () => {
+        marker.setVisible(false);
+    
+        const place = autocomplete.getPlace();
+    
+        if (!place.geometry || !place.geometry.location) {
+          // User entered the name of a Place that was not suggested and
+          // pressed the Enter key, or the Place Details request failed.
+          window.alert("No details available for input: '" + place.name + "'");
+          return;
+        }
+
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+          }
+
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+        place.formatted_address;
+    });
+
 }
 window.initMap = initMap;
