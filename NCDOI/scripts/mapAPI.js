b6c39a318650802dde4e4fc86f7a7b7f034ad9fa
IZALZO <rmauney6@uncc.edu>
2022-11-17 15:19:27 -0500

    SFSF

--- NCDOI/scripts/mapAPI.js
+++ NCDOI/scripts/mapAPI.js
@@ -1,11 +1,20 @@
 //AIzaSyA1-_yQ50-uJIIrykNyDlcktIH8wNhxkbE
+
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 35.2271, lng: 80.8431 },
-    zoom: 8,
-  });
+    map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 35.7079, lng: -79.8136 },
+    zoom: 7,
+    restriction:{
+        latLngBounds:{
+            north: 38,
+            south: 33,
+            east: -74,
+            west: -86
+            }
+        }
+    });
 }
 
 window.initMap = initMap;
