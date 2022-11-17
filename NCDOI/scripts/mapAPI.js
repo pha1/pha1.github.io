d0051ff4b9a0075bd63165395ae033f470b32a29
IZALZO <rmauney6@uncc.edu>
2022-11-17 18:15:22 -0500

    Update mapAPI.js

--- NCDOI/scripts/mapAPI.js
+++ NCDOI/scripts/mapAPI.js
@@ -2,19 +2,19 @@
 
 let map;
 
-          function initMap() {
-            map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: 35.7079, lng: -79.8136 },
-            zoom: 7,
-            restriction:{
-              latLngBounds:{
-                north: 38,
-                south: 33,
-                east: -74,
-                west: -86
-                }
-              }
-            });
-            map.data.loadGeoJson('files/NC_Counties.geojson');
-          }
-          window.initMap = initMap;
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 35.7079, lng: -79.8136 },
+        zoom: 7,
+        restriction:{
+            latLngBounds:{
+            north: 38,
+            south: 33,
+            east: -74,
+            west: -86
+            }
+        }
+    });
+    map.data.loadGeoJson('files/NC_Counties.geojson');
+}
+window.initMap = initMap;
