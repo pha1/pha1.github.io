6a06069a8988b84e6a19eae6466310796eb0f979
pha1 <89549278+pha1@users.noreply.github.com>
2022-12-07 13:32:43 -0500

    Database config and some changes
    Added the database config, and changed the script import from firebase-database to firebase-firestore

--- NCDOI/scripts/admin.js
+++ NCDOI/scripts/admin.js
@@ -1,17 +1,18 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+  apiKey: "AIzaSyA4UoQh1ErFeB23aUlww4pYCvliXIS6I60",
+  authDomain: "inspector-locator-ef89c.firebaseapp.com",
+  projectId: "inspector-locator-ef89c",
+  storageBucket: "inspector-locator-ef89c.appspot.com",
+  messagingSenderId: "253182318432",
+  appId: "1:253182318432:web:cd18b909058d28e81ff005",
+  measurementId: "G-16PTXW98PR"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   // Initialize variables
   const auth = firebase.auth()
-  const database = firebase.database()
+  const database = firebase.firestore()
   
   // Set up our register function
   function register () {
@@ -40,8 +41,6 @@ var firebaseConfig = {
       var user_data = {
         email : email,
         full_name : full_name,
-        favourite_song : favourite_song,
-        milk_before_cereal : milk_before_cereal,
         last_login : Date.now()
       }
   
