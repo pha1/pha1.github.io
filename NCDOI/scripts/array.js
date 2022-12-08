9065f9e96e9abbceb8276c0e29a00320595e9f2b
pha1 <89549278+pha1@users.noreply.github.com>
2022-12-08 16:26:22 -0500

    Update array.js

--- NCDOI/scripts/array.js
+++ NCDOI/scripts/array.js
@@ -3,13 +3,13 @@
 var inspectors = [];
 
 var firebaseConfig = {
-    apiKey: "AIzaSyA4UoQh1ErFeB23aUlww4pYCvliXIS6I60",
-    authDomain: "inspector-locator-ef89c.firebaseapp.com",
-    projectId: "inspector-locator-ef89c",
-    storageBucket: "inspector-locator-ef89c.appspot.com",
-    messagingSenderId: "253182318432",
-    appId: "1:253182318432:web:cd18b909058d28e81ff005",
-    measurementId: "G-16PTXW98PR"
+    apiKey: "AIzaSyAoNHpW6B3UNJ0KV8DXKhoMSdOJ3m4bkNY",
+    authDomain: "inspectorlocator.firebaseapp.com",
+    projectId: "inspectorlocator",
+    storageBucket: "inspectorlocator.appspot.com",
+    messagingSenderId: "373749984697",
+    appId: "1:373749984697:web:3a894f81fc10e3bd4bda68",
+    measurementId: "G-GE2S58262L"
     };
 
     // Initialize Firebase
@@ -21,14 +21,28 @@ const db = firebaseApp.firestore();
 
 const inspectorCollection = db.collection("inspectors");
 
+inspectorCollection.get("Gisau75CRdpkl7EFC5").get().then((doc) => {
+    if (doc.exists) {
+        console.log("Document data: " + doc.data);
+        var obj = {}
+        obj['CEO ID'] = doc.data().ceoId
+        obj['Trade'] = doc.data().trade
+        inspectors.push(obj);
+    } else {
+        console.log("Document doesn't exist");
+    }
+});
+
+/*
 inspectorCollection.get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
         //console.log(doc.id, " => ", doc.data());
         var obj = {}
-      obj['CEO ID'] = doc.data()["CEO ID"]
-      obj['Trade'] = doc.data().Trade
+      obj['CEO ID'] = doc.data().ceoId
+      obj['Trade'] = doc.data().trade
         inspectors.push(obj);
         console.log(inspectors);
     });
 });
+*/
