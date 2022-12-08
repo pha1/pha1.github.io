eed2ed229291152e742a89b7d5b5989a5b0e73be
IZALZO <rmauney6@uncc.edu>
2022-12-08 13:41:38 -0500

    yiyiyi

---
+++ NCDOI/scripts/jsonUpdate.js
@@ -0,0 +1,14 @@
+function updateJSON(){
+
+}
+function addJSON(){
+    
+}
+
+function showDiv1() {
+    document.getElementById('add').style.display = "block";
+}
+
+ function showDiv2() {
+    document.getElementById('edit').style.display = "block";
+}
