bdaaba4f2383ee7346f9fee275aa4d9d9726ab80
IZALZO <rmauney6@uncc.edu>
2022-12-08 17:29:16 -0500

    Update array.js

--- NCDOI/scripts/array.js
+++ NCDOI/scripts/array.js
@@ -21,60 +21,14 @@ const db = firebaseApp.firestore();
 
 const inspectorCollection = db.collection("inspectors");
 
-/*
-// Tested one document to object and added it into array -> Works
-inspectorCollection.doc("00Gisau75CRdpkl7EFC5").get().then((doc) => {
-    inspectors = [];
-    if (doc.exists) {
-        var obj = {}
-        obj['CEO ID'] = doc.data().ceoId
-        obj['First Name'] = doc.data().firstName
-        obj['Last Name'] = doc.data().lastName
-        obj['Cert No'] = doc.data().certNo
-        obj['Type'] = doc.data().type
-        obj['Trade'] = doc.data().trade
-        obj['Level'] = doc.data().level
-        obj['Exp Date'] = doc.data().expDate
-        obj['Employer'] = doc.data().employer
-        obj['Emp Phone'] = doc.data().empPhone
-        obj['County'] = doc.data().county
-        obj['Address'] = doc.data().address
-        obj['City'] = doc.data().city
-        obj['State'] = doc.data().state
-        obj['Zip Code'] = doc.data().zipCode
-        obj['Primary contact email'] = doc.data().primaryContactEmail
-        inspectors.push(obj);
-        console.log(inspectors);
-    } else {
-        console.log("Document doesn't exist");
-    }
-});
-*/
-
-/*
 inspectorCollection.get().then((querySnapshot) => {
-    inspectors = [];
     querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
         //console.log(doc.id, " => ", doc.data());
         var obj = {}
-        obj['CEO ID'] = doc.data().ceoId
-        obj['First Name'] = doc.data().firstName
-        obj['Last Name'] = doc.data().lastName
-        obj['Cert No'] = doc.data().certNo
-        obj['Type'] = doc.data().type
-        obj['Trade'] = doc.data().trade
-        obj['Level'] = doc.data().level
-        obj['Exp Date'] = doc.data().expDate
-        obj['Employer'] = doc.data().employer
-        obj['Emp Phone'] = doc.data().empPhone
-        obj['County'] = doc.data().county
-        obj['Address'] = doc.data().address
-        obj['City'] = doc.data().city
-        obj['State'] = doc.data().state
-        obj['Zip Code'] = doc.data().zipCode
-        obj['Primary contact email'] = doc.data().primaryContactEmail
+      obj['CEO ID'] = doc.data()["CEO ID"]
+      obj['Trade'] = doc.data().Trade
         inspectors.push(obj);
         console.log(inspectors);
     });
 });
-*/
