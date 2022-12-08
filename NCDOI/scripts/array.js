e7b10903459cff6c262cf6210c96e73d270c7bc6
pha1 <89549278+pha1@users.noreply.github.com>
2022-12-08 14:36:00 -0500

    Create array.js

---
+++ NCDOI/scripts/array.js
@@ -0,0 +1,31 @@
+
+src="https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js"
+src="https://www.gstatic.com/firebasejs/8.6.8/firebase-auth.js"
+src="https://www.gstatic.com/firebasejs/8.6.8/firebase-firestore.js"
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA4UoQh1ErFeB23aUlww4pYCvliXIS6I60",
+    authDomain: "inspector-locator-ef89c.firebaseapp.com",
+    projectId: "inspector-locator-ef89c",
+    storageBucket: "inspector-locator-ef89c.appspot.com",
+    messagingSenderId: "253182318432",
+    appId: "1:253182318432:web:cd18b909058d28e81ff005",
+    measurementId: "G-16PTXW98PR"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    // Initialize variables
+    const auth = firebase.auth()
+    const database = firebase.firestore();
+
+    const inspectorCollection = db.collection("inspectors");
+
+    inspectorCollection.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    });
