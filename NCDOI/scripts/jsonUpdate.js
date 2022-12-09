c44ff6a58e6db2448fed0c061073092f85096a04
pha1 <89549278+pha1@users.noreply.github.com>
2022-12-08 19:13:28 -0500

    Update jsonUpdate.js

--- NCDOI/scripts/jsonUpdate.js
+++ NCDOI/scripts/jsonUpdate.js
@@ -1,8 +1,60 @@
+var ceoId;
+var firstName;
+var lastName;
+var certNo;
+var type;
+var trade;
+var level;
+var expDate;
+var employer;
+var empPhone;
+var county;
+var address;
+var city;
+var state;
+var zipCode;
+var email;
+
 function deleteJSON(){
 
 }
 function addJSON(){
-    
+
+    ceoId = document.getElementById("CEO ID");
+    firstName = document.getElementById("First Name");
+    lastName = document.getElementById("Last Name");
+    certNo = document.getElementById("Cert No");
+    type = document.getElementById("Type");
+    trade = document.getElementById("Trade");
+    level = document.getElementById("Level");
+    expDate = document.getElementById("Exp Date");
+    employer = document.getElementById("Employer");
+    empPhone = document.getElementById("Emp Phone");
+    county = document.getElementById("County");
+    address = document.getElementById("Address");
+    city = document.getElementById("City");
+    state = document.getElementById("State");
+    zipCode = document.getElementById("Zip Code");
+    email = document.getElementById("Primary contact email");
+
+    firebase.database().ref('users/' + userId).set({
+        ["CEO ID"]: ceoId,
+        ["First Name"]: firstName,
+        ["Last Name"]: lastName,
+        ["Cert No"]:certNo,
+        Type: type,
+        Trade: trade,
+        Level: level,
+        ["Exp Date"]: expDate,
+        Employer: employer,
+        ["Emp Phone"]: empPhone,
+        County: county,
+        Address: address,
+        City: city,
+        State: state,
+        ["Zip Code"]: zipCode,
+        Email: email
+    });
 }
 
 function showDiv1() {
