bf869e189bac9896fb2eef48e6522f9da85c56e1
pha1 <89549278+pha1@users.noreply.github.com>
2022-12-08 22:32:45 -0500

    Update admin.js

--- NCDOI/scripts/admin.js
+++ NCDOI/scripts/admin.js
@@ -71,7 +71,7 @@ var firebaseConfig = {
   
     // Validate input fields
     if (validate_email(email) == false || validate_password(password) == false) {
-      alert('Email or Password does not meet requirments')
+      messageBox.innerHTML = 'Email or Password does not meet requirments';
       return
       // Don't continue running the code
     }
