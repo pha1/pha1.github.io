5cad5ad004742ad93cf2450c9d93cd754a4f9a0f
pha1 <89549278+pha1@users.noreply.github.com>
2022-12-08 19:27:21 -0500

    Update jsonUpdate.js

--- NCDOI/scripts/jsonUpdate.js
+++ NCDOI/scripts/jsonUpdate.js
@@ -17,6 +17,16 @@ var state;
 var zipCode;
 var email;
 
+var firebaseConfig = {
+    apiKey: "AIzaSyA4UoQh1ErFeB23aUlww4pYCvliXIS6I60",
+    authDomain: "inspector-locator-ef89c.firebaseapp.com",
+    projectId: "inspector-locator-ef89c",
+    storageBucket: "inspector-locator-ef89c.appspot.com",
+    messagingSenderId: "253182318432",
+    appId: "1:253182318432:web:cd18b909058d28e81ff005",
+    measurementId: "G-16PTXW98PR"
+    };
+
 firebase.initializeApp(firebaseConfig);
 
     // Initialize variables
