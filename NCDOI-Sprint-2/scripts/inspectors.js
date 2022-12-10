0029226bd1f2edf777b4fd92aaa26a547863c07d
pha1 <89549278+pha1@users.noreply.github.com>
2022-12-10 14:56:57 -0500

    Update inspectors.js

--- NCDOI-Sprint-2/scripts/inspectors.js
+++ NCDOI-Sprint-2/scripts/inspectors.js
@@ -70,7 +70,7 @@ let submitForm = function () {
     //for loop to iterate through origanal array and place items in new array sorted by county
     for(var i = 0; i < parsedJSON.length; i++) {
         if(searchValue === parsedJSON[i].County){
-            resultJSON[x].push(parsedJSON[i]);
+            resultJSON.push(parsedJSON[i]);
             x++;
         }
     }
