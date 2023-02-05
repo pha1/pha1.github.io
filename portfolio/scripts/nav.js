ebf3d54c8448fc1d37a55df75c6c427362828b61
pha1 <89549278+pha1@users.noreply.github.com>
2023-02-05 17:51:02 -0500

    Edit script src
    Edit "scripts/" into the script src paths

--- portfolio/scripts/nav.js
+++ portfolio/scripts/nav.js
@@ -1,7 +1,7 @@
 fetch('nav.html')
 .then(res => res.text())
 .then(text => {
-    let oldelem = document.querySelector("#replace_with_navbar");
+    let oldelem = document.querySelector("script#replace_with_navbar");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem, oldelem);
