33f4502fcc13cae86c6fae3b4db83ce74f50c87f
pha1 <89549278+pha1@users.noreply.github.com>
2023-02-06 00:53:51 -0500

    Navbar fix attempt

--- portfolio/scripts/nav.js
+++ portfolio/scripts/nav.js
@@ -1,9 +1,13 @@
 fetch('nav.html')
 .then(res => res.text())
 .then(text => {
+    let oldelem = document.getElementById("replace_with_navbar");
+    oldelem.innerHTML = text;
+    /*
     let oldelem = document.querySelector("script#replace_with_navbar");
     let newelem = document.createElement("div");
     newelem.setAttribute("id", "navbar");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem, oldelem);
+    */
 })
