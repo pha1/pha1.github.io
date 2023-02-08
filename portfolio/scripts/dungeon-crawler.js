5cdc389ecc57509db98bc2b5a03053ce32902dfc
pha1 <89549278+pha1@users.noreply.github.com>
2023-02-08 10:54:03 -0500

    Dungeon Crawler setup
    Created Dungeon Crawler JS file, added comments. Created div for game container.

---
+++ portfolio/scripts/dungeon-crawler.js
@@ -0,0 +1,11 @@
+/** 
+ * This is the JavaScript file for Dungeon Crawler.
+ * The functionality of the game will all be placed here.
+ * 
+ * Things to look out for:
+ *      Battle System - Health, Attack
+ *      Loot System - Gold, Items
+ *      Movement - Different Paths?
+ *      Leveling System - Certain level requirements?
+ *      Shop/Market - Buy health, items?
+ */
